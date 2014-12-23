6c7ed08b3bcd35921d10f142bdd10c26fb53eaaa
Daan Rijks <daanrijks@gmail.com>
2014-12-23 21:54:00 +0100

    Small stuff

--- js/api.js
+++ js/api.js
@@ -33,38 +33,9 @@ function displayVoteTally() {
         });
 
         $resultList.appendTo($('#votetally.status'));
-    }).error(function(){
+    }).fail(function(){
         $('<p></p>', {
-            text: "Couldn't load the data! Go bother ijks about it."
+            text: "Error loading data! Go bother ~ijks about it."
         }).appendTo($('#votetally.status'));
     });
 }
-
-// {
-//     "color": {
-//         "da2c43": 0,
-//         "ec1d25": 0,
-//         "ed2939": 1,
-//         "cf4759": 0
-//     },
-//     "crt-effect": {
-//         "yes": 0,
-//         "no": 1
-//     }
-// }
-
-// becomes:
-// <ul>
-//     <li>color</li>
-//     <ul>
-//         <li>da2c43: 0</li>
-//         <li>ec1d25: 0</li>
-//         <li>ed2939: 1</li>
-//         <li>cf4759: 0</li>
-//     </ul>
-//     <li>crt-effect</li>
-//     <ul>
-//         <li>yes: 0</li>
-//         <li>no: 1</li>
-//     </ul>
-// </ul>
